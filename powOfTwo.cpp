9d81b7594701ff74a36bc1afe82f1d533a2adc6c
Anshita-C <anshitabhd@gmail.com>
2026-08-06 23:58:27 +0530

    code to determine whether a number is a power of two using bitwise operators

--- powOfTwo.cpp
+++ powOfTwo.cpp
@@ -2,28 +2,40 @@
 using namespace std;
 
 int main(){
+    // Method 1: using loops
+    // int n;
+    // cout << "your number: ";
+    // cin >> n;
+
+    // if (n <= 0) {
+    //     cout << "It's not a power of two\n";
+    //     return 0;
+    // }
+    
+    // int num=1;
+    // bool powOfTwo=true;
+    // while (num<=n){
+    //     if(n%num!=0){
+    //         powOfTwo=false;
+    //     }
+    //     num*=2;
+    // }
+    // if (powOfTwo){
+    //     cout << "It's a power of two\n";
+    //     } else {
+    //         cout << "It's not a power of two\n";
+    //     }
+
+
+    //Method 2: Using bitwise operators
     int n;
     cout << "your number: ";
     cin >> n;
-
-    if (n <= 0) {
-        cout << "It's not a power of two\n";
-        return 0;
-    }
-    
-    int num=1;
-    bool powOfTwo=true;
-    while (num<=n){
-        if(n%num!=0){
-            powOfTwo=false;
-        }
-        num*=2;
-    }
-    if (powOfTwo){
+    if(n>0 && (n & (n-1)) == 0){
         cout << "It's a power of two\n";
-        } else {
-            cout << "It's not a power of two\n";
-        }
+    } else{
+        cout << "It's not a power of two\n";
+    }
 
 
     return 0;
