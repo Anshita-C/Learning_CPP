2d5ea7bf0cbd48cf639fae75c7f553d05748a4f9
Anshita-C <anshitabhd@gmail.com>
2026-08-10 23:30:50 +0530

    largest number in a given array

---
+++ largestNum.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+     int nums[] = {12, 1, -24, 34, 27};
+    int size = 6;
+    int largest=INT_MIN; //= -infinity
+
+
+    for(int i=0; i<size; i++){
+        if(nums[i]>largest){
+            largest = nums[i];
+        }
+    }
+    cout << "largest = " << largest << endl;
+
+
+    return 0;
+}
